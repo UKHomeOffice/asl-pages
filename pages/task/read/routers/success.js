e65f1dd9a8ffccf0f5402efeefd9115905a4602b
Joe Fitter <joefitter@hotmail.com>
2019-06-10 11:42:34 +0100

    Fix success pages
    * Use pel for establishment, role and place updates
    * dont clear status update from session, so success page can be reloaded
    * model status fixed in workflow - now status when task submitted

--- pages/task/read/routers/success.js
+++ pages/task/read/routers/success.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { merge, get, set, pick } = require('lodash');
+const { merge, get, set } = require('lodash');
 const successContent = require('../../../common/content/success-messages');
 
 module.exports = () => {
@@ -14,7 +14,6 @@ module.exports = () => {
   app.use((req, res, next) => {
     const status = get(req.session, `form.${req.model.id}.values.status`);
     req.status = status;
-
     res.locals.static.profile = req.task.data.changedBy;
     next();
   });
@@ -23,15 +22,14 @@ module.exports = () => {
     const model = req.task.data.model === 'place' || req.task.data.model === 'role' || req.task.data.model === 'establishment'
       ? 'pel'
       : req.task.data.model;
-    console.log(model, req.status);
     const content = get(successContent, `${model}.${req.task.type}.${req.status}`, successContent.fallback);
     merge(res.locals.static.content, { success: content });
     next();
   });
 
   app.get('/', (req, res, next) => {
-    const values = get(req.session, `form[${req.model.id}].values`);
-    set(req.session, `form[${req.model.id}].values`, pick(values, 'status'));
+    const status = get(req.session, `form[${req.model.id}].values.status`);
+    set(req.session, `form.${req.model.id}.values.status`, status);
     next();
   });
 
