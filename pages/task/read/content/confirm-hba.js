3473b29f371490f7b45aa4da51cdef5d79da873b
Ishaq <156688098+farrukhishaq@users.noreply.github.com>
2024-05-08 13:45:40 +0100

    content added proposed PPL license holder, for confirm-hba.jsx page.

--- pages/task/read/content/confirm-hba.js
+++ pages/task/read/content/confirm-hba.js
@@ -19,6 +19,9 @@ module.exports = merge({}, baseContent, {
     applicant: {
       label: 'Applicant:'
     },
+    proposedApplicant: {
+      label: 'Proposed PPL licence holder:'
+    },
     pplHolder: {
       label: 'PPL holder:'
     },
--- pages/task/read/views/confirm-hba.jsx
+++ pages/task/read/views/confirm-hba.jsx
@@ -10,73 +10,6 @@ import {
 import { Warning } from '../../../common/components/warning';
 import { getTypeAdjustedWording, isAmendment } from './adjusted-wording';
 
-const EstablishmentNames = ({task, establishment}) => {
-  if (task.data.action === 'transfer') {
-    const {from, to} = task.data.meta.establishment;
-    return <>
-      <p>
-        <strong>
-          <Snippet>fields.currentEstablishment.label</Snippet>
-        </strong>
-        <br />
-        {from.name}
-      </p>
-      <p>
-        <strong>
-          <Snippet>fields.proposedEstablishment.label</Snippet>
-        </strong>
-        <br />
-        {to.name}
-      </p>
-    </>;
-  } else {
-    return <p>
-      <strong>
-        <Snippet>fields.establishment.label</Snippet>
-      </strong>
-      <br />
-      {establishment.name}
-    </p>;
-  }
-};
-
-const LicenceNames = ({task, licenceHolder}) => {
-  if (task.data.action === 'update') {
-    return <>
-      <p>
-        <strong>
-          <Snippet>fields.currentPPLLicenceHolder.label</Snippet>
-        </strong>
-        <br />
-        {task.data.profile.name}
-      </p>
-      <p>
-        <strong>
-          <Snippet>fields.proposedPPLLicenceHolder.label</Snippet>
-        </strong>
-        <br />
-        {licenceHolder.name}
-      </p>
-    </>;
-  } else if (task.data.action === 'transfer') {
-    return <p>
-      <strong>
-        <Snippet>fields.pplHolder.label</Snippet>
-      </strong>
-      <br />
-      {licenceHolder.name}
-    </p>;
-  } else {
-    return <p>
-      <strong>
-        <Snippet>fields.applicant.label</Snippet>
-      </strong>
-      <br />
-      {licenceHolder.name}
-    </p>;
-  }
-};
-
 const ConfirmHba = ({ establishment, licenceHolder, hba, task }) => {
   let action = task.data.action;
   const uploadType = getTypeAdjustedWording(action, task.type);
@@ -91,8 +24,27 @@ const ConfirmHba = ({ establishment, licenceHolder, hba, task }) => {
           title={<Snippet>title</Snippet>}
           subtitle={<Snippet>{`tasks.${task.data.model}.${action}`}</Snippet>}
         />
-        <EstablishmentNames establishment={establishment} task={task} />
-        <LicenceNames licenceHolder={licenceHolder} task={task} />
+        <p>
+          <strong>
+            <Snippet>fields.establishment.label</Snippet>
+          </strong>
+          <br />
+          {establishment.name}
+        </p>
+        <p>
+          <strong>
+            <Snippet>fields.applicant.label</Snippet>
+          </strong>
+          <br />
+          {task.data.profile.name}
+        </p>
+        <p>
+          <strong>
+            <Snippet>fields.proposedApplicant.label</Snippet>
+          </strong>
+          <br />
+          {licenceHolder.name}
+        </p>
         <p>
           <strong>
             <Snippet>fields.hbaFilename.label</Snippet>
@@ -109,8 +61,8 @@ const ConfirmHba = ({ establishment, licenceHolder, hba, task }) => {
 };
 
 const mapStateToProps = ({
-  static: { task, establishment, licenceHolder, hba }
-}) => ({
+                           static: { task, establishment, licenceHolder, hba }
+                         }) => ({
   task,
   establishment,
   licenceHolder,
