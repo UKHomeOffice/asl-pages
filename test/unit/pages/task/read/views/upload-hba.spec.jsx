99d67f0f71a159ca6b236d88048fc0d4082af327
Ishaq <156688098+farrukhishaq@users.noreply.github.com>
2024-05-09 13:19:54 +0100

    test aded for upload-hba transfer journey.

--- test/unit/pages/task/read/views/upload-hba.spec.jsx
+++ test/unit/pages/task/read/views/upload-hba.spec.jsx
@@ -39,4 +39,19 @@ describe('HBA Upload intro', () => {
     expect(paragraphs[1].children[0].data).toEqual(
       'The HBA will be visible to ASRU only.');
   });
+
+  test('Mustache framework append Transfer into content', () => {
+    const wrapper = render(
+      <div>
+        <Snippet content={content} action='transfer' type='transfer'>intro</Snippet>
+      </div>
+    );
+
+    const paragraphs = wrapper.find('p');
+    expect(paragraphs.length).toEqual(2);
+    expect(paragraphs[0].children[0].data).toEqual(
+      'To transfer the licence you must upload the PPL assessment form containing the harm benefit analysis (HBA) for this transfer.');
+    expect(paragraphs[1].children[0].data).toEqual(
+      'The HBA will be visible to ASRU only.');
+  });
 });
