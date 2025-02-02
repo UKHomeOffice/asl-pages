72500df6e1b3110584c424acb6e1f77e40ed4900
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-02-02 22:40:38 +0000

    styling changes

--- pages/role/named-person-mvp/before-you-apply/views/index.jsx
+++ pages/role/named-person-mvp/before-you-apply/views/index.jsx
@@ -7,40 +7,42 @@ const Page = () => {
   const { profile, trainingDashboardUrl } = useSelector(state => state.static, shallowEqual);
 
   return (
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-two-thirds">
-        <Form cancelLink="profile.read">
-          <span className="govuk-caption-l">{profile.firstName} {profile.lastName}</span>
-          <Header title={<Snippet>title</Snippet>}/>
-          { trainingDashboardUrl && <p className="govuk-body">{<Snippet>beforeYouNominateIntro</Snippet>}</p> }
-        </Form>
-      </div>
+    <div>
+      <span className="govuk-caption-l">{profile.firstName} {profile.lastName}</span>
 
-      <div className="govuk-grid-column-one-third">
-        <div className="x-govuk-related-navigation">
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-two-thirds">
+          <Form cancelLink="profile.read">
+            <Header title={<Snippet>title</Snippet>}/>
+            { trainingDashboardUrl && <p className="govuk-body">{<Snippet>beforeYouNominateIntro</Snippet>}</p> }
+          </Form>
+        </div>
 
-          <p><h2 className="govuk-heading-m"><Snippet>supportingGuidanceTitle</Snippet></h2></p>
+        <div className="govuk-grid-column-one-third">
+          <div className="x-govuk-related-navigation">
 
-          <p className="govuk-body">
-            <a className="govuk-link" href="https://www.gov.uk/guidance/research-and-testing-using-animals#add-a-named-person-role">Adding named person roles</a>
-          </p>
+            <p><h2 className="govuk-heading-m"><Snippet>supportingGuidanceTitle</Snippet></h2></p>
 
-          {/* Links not working correctly */}
-          <p className="govuk-body">
-            <a className="govuk-link" href="https://www.gov.uk/government/publications/conflict-of-interest-declaration-form-aspa-1986">Make a conflict of interest declaration</a>
-          </p>
+            <p className="govuk-body">
+              <a className="govuk-link" href="https://www.gov.uk/guidance/research-and-testing-using-animals#add-a-named-person-role">Adding named person roles</a>
+            </p>
 
-          <p className="govuk-body">
-            <a className="govuk-link" href="https://www.gov.uk/government/publications/training-and-development-under-the-animals-scientific-procedures-act">Guidance on training and continuous professional development (CPD) under ASPA</a>
-          </p>
+            {/* Links not working correctly */}
+            <p className="govuk-body">
+              <a className="govuk-link" href="https://www.gov.uk/government/publications/conflict-of-interest-declaration-form-aspa-1986">Make a conflict of interest declaration</a>
+            </p>
 
-          <p className="govuk-body">
-            <a className="govuk-link" href="https://www.gov.uk/government/publications/the-operation-of-the-animals-scientific-procedures-act-1986">Guidance on the operation of the Animals (Scientific Procedures) Act 1986</a>
-          </p>
+            <p className="govuk-body">
+              <a className="govuk-link" href="https://www.gov.uk/government/publications/training-and-development-under-the-animals-scientific-procedures-act">Guidance on training and continuous professional development (CPD) under ASPA</a>
+            </p>
+
+            <p className="govuk-body">
+              <a className="govuk-link" href="https://www.gov.uk/government/publications/the-operation-of-the-animals-scientific-procedures-act-1986">Guidance on the operation of the Animals (Scientific Procedures) Act 1986</a>
+            </p>
+          </div>
         </div>
       </div>
     </div>
-
   );
 };
 
