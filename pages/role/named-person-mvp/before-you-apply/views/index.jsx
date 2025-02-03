e7da60a08f6d2fda2ab5c0bf6f19856ed20ccbd5
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-02-03 14:06:07 +0000

    added xgov styling

--- pages/role/named-person-mvp/before-you-apply/views/index.jsx
+++ pages/role/named-person-mvp/before-you-apply/views/index.jsx
@@ -40,6 +40,27 @@ const Page = () => {
               <a className="govuk-link" href="https://www.gov.uk/government/publications/the-operation-of-the-animals-scientific-procedures-act-1986">Guidance on the operation of the Animals (Scientific Procedures) Act 1986</a>
             </p>
           </div>
+
+          <div className="x-govuk-related-navigation">
+            <nav className="x-govuk-related-navigation__nav-section" role="navigation" aria-labelledby="related-navigation-related-content">
+              <h2 className="x-govuk-related-navigation__main-heading" id="related-navigation-related-content"><Snippet>supportingGuidanceTitle</Snippet></h2>
+
+              <ul className="x-govuk-related-navigation__link-list">
+                <li className="x-govuk-related-navigation__link">
+                  <a className="govuk-link x-govuk-related-navigation__section-link x-govuk-related-navigation__section-link--other" href="https://www.gov.uk/guidance/research-and-testing-using-animals#add-a-named-person-role">Adding named person roles</a>
+                </li>
+                <li className="x-govuk-related-navigation__link">
+                  <a className="govuk-link x-govuk-related-navigation__section-link x-govuk-related-navigation__section-link--other" href="https://www.gov.uk/government/publications/conflict-of-interest-declaration-form-aspa-1986">Make a conflict of interest declaration</a>
+                </li>
+                <li className="x-govuk-related-navigation__link">
+                  <a className="govuk-link x-govuk-related-navigation__section-link x-govuk-related-navigation__section-link--other" href="https://www.gov.uk/government/publications/training-and-development-under-the-animals-scientific-procedures-act">Guidance on training and continuous professional development (CPD) under ASPA</a>
+                </li>
+                <li className="x-govuk-related-navigation__link">
+                  <a className="govuk-link x-govuk-related-navigation__section-link x-govuk-related-navigation__section-link--other" href="https://www.gov.uk/government/publications/the-operation-of-the-animals-scientific-procedures-act-1986">Guidance on the operation of the Animals (Scientific Procedures) Act 1986</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </div>
